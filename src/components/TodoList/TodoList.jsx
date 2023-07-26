faf59f330ffa742b3c886cbe5dea43cc8c33ffcd
Chanwoong(Noah) Park <135056537+Noah1215@users.noreply.github.com>
2023-07-26 09:45:27 -0400

    First-commit-components

---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import AddTodo from "../AddTodo/AddTodo";
+import Todo from "../Todo/Todo";
+import styles from "./TodoList.module.css";
+
+const TodoList = ({ filter }) => {
+  const [todos, setTodos] = useState([
+    { id: "123", text: "Shopping", status: "active" },
+    { id: "124", text: "Studying", status: "active" },
+  ]);
+  const handleAdd = (todo) => {
+    setTodos([...todos, todo]);
+  };
+  const handleUpdate = (updated) => {
+    setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
+  };
+  const handleDelete = (deleted) => {
+    setTodos(todos.filter((t) => t.id !== deleted.id));
+  };
+  const filtered = getFilteredItems(todos, filter);
+  return (
+    <section className={styles.container}>
+      <ul className={styles.list}>
+        {filtered.map((item) => (
+          <Todo
+            key={item.id}
+            todo={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          />
+        ))}
+      </ul>
+      <AddTodo onAdd={handleAdd} />
+    </section>
+  );
+};
+
+function getFilteredItems(todos, filter) {
+  if (filter === "all") {
+    return todos;
+  }
+
+  return todos.filter((todo) => todo.status === filter);
+}
+
+export default TodoList;
